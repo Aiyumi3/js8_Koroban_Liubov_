6696662ce1c523df7ead4e612881fa614d26e357
Aiyumi Sunshine <62023811+Aiyumi3@users.noreply.github.com>
2020-11-13 22:27:29 +0200

    Update task3.js

--- js/task3.js
+++ js/task3.js
@@ -1,37 +1,23 @@
 class Marker {
-    //amOfInk = 55.7;
     constructor() {
         this.color = '#' + Math.random().toString(16).slice(2, 8);
         this.numOfInk = 55.7;
     }
     print() {
-        return document.getElementById('p').style.color = this.color;
-        
+        return document.getElementById('p').style.color = this.color; 
    }
-        //do {
-         //return document.getElementById('p').style.color = 'black';
-        //}
-       //while 
-   amountOnclick(){
-       
-       $('#b')
-.data('counter', 0)                            
-.click(function() {
-    let counter = $(this).data('counter');    
-    $(this).data('counter', counter + 1);        
-    alert($(this).data('counter'));             
-});
-       
+   amountOnclick(){ 
        let patt1 = /\S/g;
        let str = document.getElementById('p').textContent;
        let result = str.match(patt1).length;
      
-         this.numOfInk -= result;
-        let n = (this.numOfInk).toFixed(1);
-       return document.getElementById('p2').innerHTML = ' amount of ink =  ' + n + ', simbols =  ' + result;
-        
-      if( n <= 0){
-          return document.getElementById('p').style.color = 'black';
+       this.numOfInk -= result;
+       let n = (this.numOfInk).toFixed(1);
+       return document.getElementById('p2').innerHTML = ' amount of ink =  ' + n ;
+        let i = 0;
+
+       while ( n <= i) {
+          return document.getElementById('p').style.color = 'red';
           $('#b').click(function(){
 	          $('#b').prop('disabled', true);
          });
@@ -39,8 +25,9 @@ class Marker {
 	          $('#p').prop('disabled', true);
          });
              
-         }
-   }
+          i++;
+       }
+    }
     reset(){
         return document.getElementById('p').style.color = 'white';
     }
@@ -50,13 +37,10 @@ class Marker {
             val.amountOnclick();
         });
         document.getElementById("p").onclick = function(){val.reset()};
-        //document.getElementById("b").onclick = function(){val.amountOnclick()};
     }
 }
 let marker1 = new Marker();
 marker1.onclick(marker1);
-//document.getElementById('b').addEventListener('click', function(){marker1.print()});
-//document.getElementById("p").onclick = function(){marker1.reset()};
 
 class filledMarker extends Marker{
     amountoOfInk = 0;
